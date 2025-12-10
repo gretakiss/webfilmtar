b2ee3667f3c0b7e6bb66b2540a970f85913db001
gretakiss <kgreta0310@gmail.com>
2025-12-10 03:35:32 +0100

    init

---
+++ src/main/java/hu/unideb/inf/webfilmtar/WebfilmtarApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.webfilmtar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfilmtarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfilmtarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/unideb/inf/webfilmtar/WebfilmtarApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.webfilmtar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfilmtarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
